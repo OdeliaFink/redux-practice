1fd552a00bd027edb0a025d4d529184af02c0fd3
OdeliaFink <odelia.finkel@gmail.com>
2023-06-27 14:18:30 -0400

    configure store

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const redux = require('redux');
+
+//redux store allows access to state via getState()
+const createStore = redux.createStore;
+
 const CAKE_ORDER = 'CAKE_ORDERED';
+const CAKE_RESTOCK = 'CAKE_RESTOCK';
 
 //action is an object with a type property
 //action creator is a function that returns an object
@@ -6,7 +12,14 @@ const CAKE_ORDER = 'CAKE_ORDERED';
 function orderCake() {
   return {
     type: CAKE_ORDER,
-    quantity: 1,
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCK,
+    payload: qty,
   };
 }
 
@@ -27,7 +40,26 @@ const reducer = (state = initialState, action) => {
         //first make a copy of state object and onnly update number of cakes without affection other properties
         numberOfCakes: state.numberOfCakes - 1,
       };
+    case CAKE_RESTOCK:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes + action.payload,
+      };
     default:
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log('initial state', store.getState());
+
+//anytime the store updates we log to console
+const unsubscribe = store.subscribe(() =>
+  console.log('updated state', store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(3));
+
+unsubscribe();
