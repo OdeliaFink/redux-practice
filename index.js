d4f29578313e628a56e12519a4afce840b35569c
OdeliaFink <odelia.finkel@gmail.com>
2023-06-27 13:50:41 -0400

    added reducer

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function orderCake() {
     quantity: 1,
   };
 }
+
+//reducers specify how the app's state changes in response to action
+//function that accepts state and action as two arguments and returns new state
+// (previousState, action ) => newState
+
+const initialState = {
+  //when the store just opens it starts with 10
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDER:
+      return {
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
