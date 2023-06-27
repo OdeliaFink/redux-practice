fe1e5fd76309e97441da8df2a7b003c27dffd890
OdeliaFink <odelia.finkel@gmail.com>
2023-06-27 13:35:54 -0400

    action and action creator added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('from index.js');
+const CAKE_ORDER = 'CAKE_ORDERED';
+
+//action is an object with a type property
+//action creator is a function that returns an object
+
+function orderCake() {
+  return {
+    type: CAKE_ORDER,
+    quantity: 1,
+  };
+}
