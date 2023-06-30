1e9098dce4b167cd9003c4eba6624bd3da92376b
OdeliaFink <odelia.finkel@gmail.com>
2023-06-29 21:37:30 -0400

    added ice cream slice

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,10 +1,12 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('../features/cake/cakeSlice');
+const icecreamReducer = require('../features/icecream/icecreamSlice');
 
 const store = configureStore({
   //specify all the reducers from slices that belong to features
   reducer: {
     cake: cakeReducer,
+    icecream: icecreamReducer,
   },
 });
 
